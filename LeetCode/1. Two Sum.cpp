9739b8c46f73139cdc3e276a6047e3502f5924d0
Mritunjay Kumar <mritunjay24357@iiitd.ac.in>
2025-12-22 19:17:55 +0530

    Update 1. Two Sum.cpp
    Updated for better runtime

--- LeetCode/1. Two Sum.cpp
+++ LeetCode/1. Two Sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < i; j++){
-                if (nums[i] + nums[j] == target){
-                    return {i, j};
-                }
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int answer = target - nums[i];
+            if (map.count(answer)) {
+                return {map[answer], i};
             }
+            map[nums[i]] = i;
         }
         return {};
     }
